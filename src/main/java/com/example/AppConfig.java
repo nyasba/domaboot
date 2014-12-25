44d5384a3c9b610112596632106da9f285c0710b
nyasba <nyasba@gmail.com>
2014-12-25 11:55:09 +0900

    コンソールログ出力の設定追加

--- src/main/java/com/example/AppConfig.java
+++ src/main/java/com/example/AppConfig.java
@@ -1,13 +1,12 @@
 package com.example;
 
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 import org.seasar.doma.jdbc.Config;
 import org.seasar.doma.jdbc.NoCacheSqlFileRepository;
 import org.seasar.doma.jdbc.SimpleDataSource;
 import org.seasar.doma.jdbc.SqlFileRepository;
 import org.seasar.doma.jdbc.dialect.Dialect;
 import org.seasar.doma.jdbc.dialect.H2Dialect;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -51,32 +50,17 @@ public class AppConfig {
     //  Doma用の設定
     // ---------------------------
 
-    @Autowired
-    private DataSourceProperties dataSourceProperties;
-
-    // H2の設定は以下を参考に実行
-    // http://scrtree.github.io/blog/2014/05/01/h2-database-howto-use/
-
-    @Bean
-    DataSource dataSource(){
-
+    DataSource realDataSource(){
         SimpleDataSource dataSource = new SimpleDataSource();
         dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
         dataSource.setUser("sa");
         return dataSource;
-
-//        return DataSourceBuilder
-//                .create(dataSourceProperties.getClassLoader())
-//                .url(dataSourceProperties.getUrl())
-//                .username(dataSourceProperties.getUsername())
-//                .password(dataSourceProperties.getPassword())
-//                .build();
     }
 
-//    @Bean
-//    DataSource dataSource(){
-//        return new Log4jdbcProxyDataSource(this.realDataSource());
-//    }
+    @Bean
+    DataSource dataSource(){
+        return new Log4jdbcProxyDataSource(this.realDataSource());
+    }
 
     @Bean
     Dialect dialect(){
