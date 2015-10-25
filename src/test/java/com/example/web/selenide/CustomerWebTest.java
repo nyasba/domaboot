ff765e00ae4745051beca7276c5fb4cfc2fb51f4
nyasba <nyasba@gmail.com>
2015-10-25 23:28:29 +0900

    登録のテストをParameterizeテストに変更して、2件の登録を行うように修正

--- src/test/java/com/example/web/selenide/CustomerWebTest.java
+++ src/test/java/com/example/web/selenide/CustomerWebTest.java
@@ -4,17 +4,24 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.example.App;
 import com.example.web.selenide.page.MainPage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import lombok.AllArgsConstructor;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@FixMethodOrder
+@RunWith(Theories.class)
 public class CustomerWebTest {
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUpClass() {
 
         // テスト対象アプリの起動
         App.main(new String[]{
@@ -27,21 +34,35 @@ public class CustomerWebTest {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
     }
 
-    @After
-    public void tearDown() {
+    @AfterClass
+    public static void tearDownClass() {
         WebDriverRunner.closeWebDriver();
     }
 
-    @Test
-    public void 剛田たけしを登録する() {
+    @Theory
+    public void 名前を登録する(Fixture fixture) {
         MainPage.open();
-        assertThat(MainPage.登録件数(), is(0));
+        assertThat(MainPage.登録件数(), is(fixture.num - 1));
 
-        MainPage.姓は("剛田");
-        MainPage.名は("たけし");
+        MainPage.姓は(fixture.lastName);
+        MainPage.名は(fixture.firstName);
         MainPage.で登録する();
 
-        assertThat(MainPage.登録件数(), is(1));
-        assertThat(MainPage.名前(1), is("剛田たけし"));
+        assertThat(MainPage.登録件数(), is(fixture.num));
+        assertThat(MainPage.名前(fixture.num), is(fixture.lastName + fixture.firstName));
     }
+
+    @DataPoints
+    public static Fixture[] fixtures = new Fixture[]{
+            new Fixture(1, "剛田", "たけし"),
+            new Fixture(2, "どら", "えもん")
+    };
+
+    @AllArgsConstructor
+    static class Fixture {
+        int num;
+        String lastName;
+        String firstName;
+    }
+
 }
